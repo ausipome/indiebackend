4ea5bf279b99e7bd2bb16621cfbcfb6c6f7c2667
ausipome <ausipome@gmail.com>
2023-01-05 15:35:31 +0000

    up

--- keystone.ts
+++ keystone.ts
@@ -44,7 +44,7 @@ export default withAuth(
   config<TypeInfo>({
     server: {
       cors: {
-        origin: [process.env.FRONTEND_URL!],
+        origin: process.env.FRONTEND_URL,
         credentials: true,
       },
     },

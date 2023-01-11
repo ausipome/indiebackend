a6d00feb281f62540811886ceccd8da50a1f8cab
ausipome <ausipome@gmail.com>
2023-01-11 10:31:32 +0000

    up

--- keystone.ts
+++ keystone.ts
@@ -21,8 +21,8 @@ const databaseURL = process.env.DATABASE_URL || 'file:./keystone.db';
 const sessionConfig = {
   maxAge: 60 * 60 * 24 * 360, // How long they stay signed in?
   secret: process.env.COOKIE_SECRET,
-  sameSite: 'none',
-  secure: true,
+  sameSite: 'true',
+  
 };
 
 const { withAuth } = createAuth({

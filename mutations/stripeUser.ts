6605e1f2889782d0cc7fb80f8b4ca940cacbe9f0
ausipome <ausipome@gmail.com>
2023-01-17 09:34:54 +0000

    up

--- mutations/stripeUser.ts
+++ mutations/stripeUser.ts
@@ -17,8 +17,7 @@ async function stripeUser(
     type: 'express',
     capabilities: {transfers: {requested: true},card_payments: {requested: true}},
     business_type: 'individual',
-    business_profile: {product_description: 'Selling clothes on the Indie Bubba marketplace'},
-    business_profile: {url: 'https://indiebubba.com'},
+    business_profile: {product_description: 'Selling clothes on the Indie Bubba marketplace',url: 'https://indiebubba.com',mcc: '5641'},
     email: email,
   }).catch(err => {
     throw new Error(err.message);
